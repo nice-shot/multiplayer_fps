fc2647413f42de1b4705e582e93cbd64ccc4d191
nice-shot <shefferu@gmail.com>
2019-10-31 14:20:58 +0200

    Created the basic shooting mechanics

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BulletController : MonoBehaviour {
+    public float initialSpeed;
+    public float lifespan;
+
+    private float creationTime;
+    private Rigidbody rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * initialSpeed;
+
+        creationTime = Time.time;
+    }
+
+    void Update() {
+        if (Time.time - creationTime > lifespan) {
+            Explode();
+        }
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        Explode();
+    }
+
+    private void Explode() {
+        print("Bullet exploded!");
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerShoot : MonoBehaviour {
+
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+
+    private Animator animator;
+
+    void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Fire1")) {
+            InitiateShoot();
+        }
+    }
+
+    private void InitiateShoot() {
+        // Start playing shoot animation
+        animator.SetTrigger("Shoot");
+    }
+
+    public void Shoot() {
+        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+    }
+}
