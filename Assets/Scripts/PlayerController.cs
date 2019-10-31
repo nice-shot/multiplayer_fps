cdac06117bb3537b52492410fa1a9052e786353b
nice-shot <shefferu@gmail.com>
2019-10-31 09:05:34 +0200

    Added basic movement scripts

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    private PlayerMotor motor;
+
+    void Awake() {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update() {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 horizontalMovement = transform.right * xMove;
+        Vector3 verticalMovement = transform.forward * zMove;
+
+        Vector3 velocity = (horizontalMovement + verticalMovement).normalized * speed;
+
+        motor.Move(velocity);
+    }
+}
---
+++ Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour {
+
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void FixedUpdate() {
+        PerformMovement();
+    }
+
+    private void PerformMovement() {
+        if (velocity != Vector3.zero) {
+            rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        }
+    }
+
+    public void Move(Vector3 velocity) {
+        this.velocity = velocity;
+    }
+
+}
