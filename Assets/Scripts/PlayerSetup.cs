b14c01743f530272ffa352af235be939bad1af0d
nice-shot <shefferu@gmail.com>
2019-10-31 11:11:20 +0200

    Added returning to the stage camera when player is disabled

--- Assets/Scripts/PlayerSetup.cs
+++ Assets/Scripts/PlayerSetup.cs
@@ -7,14 +7,25 @@ public class PlayerSetup : NetworkBehaviour {
 
     public Behaviour[] componentsToDisable;
 
+    private Camera sceneCamera;
+
     void Start() {
         // Disable irrelevant components
-        if (!isLocalPlayer) {
+        if (isLocalPlayer) {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null) {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        } else {
             foreach (Behaviour component in componentsToDisable) {
                 component.enabled = false;
             }
-        } else {
-            Camera.main.gameObject.SetActive(false);
+        }
+    }
+
+    void OnDisable() {
+        if (sceneCamera != null) {
+            sceneCamera.gameObject.SetActive(true);
         }
     }
 
