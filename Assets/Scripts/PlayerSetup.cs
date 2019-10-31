dc903188d4223d2f9c29a2a79826d83d82ae20b7
nice-shot <shefferu@gmail.com>
2019-10-31 10:53:48 +0200

    Added player as a network entity and cleanup script for spawning

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour {
+
+    public Behaviour[] componentsToDisable;
+
+    void Start() {
+        // Disable irrelevant components
+        if (!isLocalPlayer) {
+            foreach (Behaviour component in componentsToDisable) {
+                component.enabled = false;
+            }
+        } else {
+            Camera.main.gameObject.SetActive(false);
+        }
+    }
+
+}
