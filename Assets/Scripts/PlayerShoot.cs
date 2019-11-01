df3b165c6bfa3ca5d183981197fd0a04e2fb53c1
nice-shot <shefferu@gmail.com>
2019-11-01 17:07:32 +0200

    Added spawning of player bullets in the server - causes delay that needs to be fixed

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 [RequireComponent(typeof(Animator))]
-public class PlayerShoot : MonoBehaviour {
+public class PlayerShoot : NetworkBehaviour {
 
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
     private Animator animator;
+    private float roundtripTimer;
 
     void Awake() {
         animator = GetComponent<Animator>();
@@ -20,17 +22,42 @@ public class PlayerShoot : MonoBehaviour {
     }
 
     void Update() {
-        if (Input.GetButtonDown("Fire1")) {
-            InitiateShoot();
+        if (isLocalPlayer && Input.GetButtonDown("Fire1")) {
+            print("Initiated shooting from player");
+            roundtripTimer = Time.time;
+            CmdPlayerShoot();
         }
     }
 
-    private void InitiateShoot() {
-        // Start playing shoot animation
+    [Command]
+    void CmdPlayerShoot() {
+        print("Got command from player to shoot");
+        // Validate and send shoot command to all clients to play the animation
+        RpcInitiateShoot();
+
+    }
+
+    [ClientRpc]
+    private void RpcInitiateShoot() {
+        print("Got RPC from server to start shooting animation");
+        print("Roundtrip time: " + (Time.time - roundtripTimer));
+        // Play shoot animation
         animator.SetTrigger("Shoot");
     }
 
     public void Shoot() {
-        Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
+        if (!isLocalPlayer) {
+            return;
+        }
+        print("Got animation trigger to shoot");
+        CmdSpawnBullet(bulletSpawn.position, bulletSpawn.rotation);
+    }
+
+    [Command]
+    private void CmdSpawnBullet(Vector3 position, Quaternion rotation) {
+        print("Got command from player to spawn the bullet");
+        // Instantiate the bullet on all clients
+        GameObject bullet = Instantiate(bulletPrefab, position, rotation);
+        NetworkServer.Spawn(bullet);
     }
 }
