b478da6548a201c18821fb8ce6bf121df7959e92
nice-shot <shefferu@gmail.com>
2019-11-02 18:07:35 +0200

    Removed a few unnecessary lines and comments

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,9 +5,7 @@ using UnityEngine.Networking;
 
 public class GameManager : MonoBehaviour {
 
-
-    // Server UI
-    public GameObject serverUIPanel;
+    // UI
     public Transform playerInfoList;
     public UIPlayerInfoController playerInfoControllerPrefab;
 
