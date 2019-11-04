9e5077111f02add267bf9aeed6ac3dbe90250dd3
nice-shot <shefferu@gmail.com>
2019-11-04 10:15:35 +0200

    Added script to allow breaking objects

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+    public GameObject brokenPrefab;
+
+    public void Brake() {
+        // Create the destoyed version of this object
+        GameObject brokenObject = Instantiate(brokenPrefab, transform.position, transform.rotation);
+        brokenObject.transform.localScale = transform.localScale;
+
+        Destroy(gameObject);
+    }
+}
