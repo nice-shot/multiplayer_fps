6ccc4e357914dbab6627f6c950e39c58237c4aaf
nice-shot <shefferu@gmail.com>
2019-11-04 10:16:10 +0200

    Added a box destruction effect that works well locally

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -7,8 +7,12 @@ using UnityEngine.Networking;
 public class BulletController : NetworkBehaviour {
     public float initialSpeed;
     public float lifespan;
+    public float explosionRadius;
+    public float explosionForce;
 
     public GameObject explosionEffect;
+    public LayerMask breakableLayerMask;
+    public LayerMask movableLayerMask;
 
     [SyncVar]
     public int localId;
@@ -18,6 +22,7 @@ public class BulletController : NetworkBehaviour {
 
     private float creationTime;
     private const string PLAYER_TAG = "Player";
+    private const string DESTRUCTIBLE_TAG = "Destructible";
 
     void Awake() {
         rb = GetComponent<Rigidbody>();
@@ -28,7 +33,7 @@ public class BulletController : NetworkBehaviour {
 
     void Update() {
         if (Time.time - creationTime > lifespan) {
-            ShowExplosion(transform.position, transform.rotation);
+            ShowExplosion(transform.position);
             Destroy(gameObject);
         }
     }
@@ -37,8 +42,8 @@ public class BulletController : NetworkBehaviour {
         // Local player sends the details to the server
         if (hasAuthority) {
             print("Bullet hit: " + collision.gameObject.name);
-            // Create explosion
-            CmdAnnounceExplosion(transform.position, transform.rotation);
+            // Send explosion information to the server
+            CmdAnnounceExplosion(transform.position);
 
             // Mark player hit
             if (collision.gameObject.CompareTag(PLAYER_TAG)) {
@@ -47,7 +52,7 @@ public class BulletController : NetworkBehaviour {
             }
 
             // Play explosion for local player
-            ShowExplosion(transform.position, transform.rotation);
+            ShowExplosion(transform.position);
         }
 
         // Maybe add double checking of collision on the server side
@@ -56,10 +61,11 @@ public class BulletController : NetworkBehaviour {
     }
 
     [Command]
-    private void CmdAnnounceExplosion(Vector3 position, Quaternion rotation) {
+    private void CmdAnnounceExplosion(Vector3 position) {
         print("Bullet exploded!");
-        RpcPlaceExplosion(position, rotation);
-        ShowExplosion(position, rotation);
+        RpcPlaceExplosion(position);
+        // Show explosion on the server
+        ShowExplosion(position);
         Destroy(gameObject);
     }
 
@@ -69,20 +75,39 @@ public class BulletController : NetworkBehaviour {
         Player player = GameManager.instance.GetPlayer(playerId);
         print("Updating hits for player: " + player + " to" + (player.hitsTaken + 1));
         player.hitsTaken += 1;
-        // The hook doesn't happen on the server so change apply this manually
+        // The hook doesn't happen on the server so apply this manually
         player.OnChangeHits(player.hitsTaken);
     }
 
     [ClientRpc]
-    private void RpcPlaceExplosion(Vector3 position, Quaternion rotation) {
+    private void RpcPlaceExplosion(Vector3 position) {
         if (!hasAuthority) {
-            ShowExplosion(position, rotation);
+            ShowExplosion(position);
         }
         Destroy(gameObject);
     }
 
-    private void ShowExplosion(Vector3 position, Quaternion rotation) {
-        Instantiate(explosionEffect, position, rotation);
+    private void ShowExplosion(Vector3 position) {
+        Instantiate(explosionEffect, position, Quaternion.identity);
+
+        // Brake objects in the explosion radius
+        Collider[] collidersToBrake = Physics.OverlapSphere(position, explosionRadius, breakableLayerMask);
+
+        foreach (Collider nearbyObject in collidersToBrake) {
+            Destructible destructable = nearbyObject.GetComponent<Destructible>();
+            if (destructable != null) {
+                destructable.Brake();
+            }
+        }
+
+        // Move objects near the explosion
+        Collider[] collidersToMove = Physics.OverlapSphere(position, explosionRadius, movableLayerMask);
+        foreach (Collider nearbyObject in collidersToMove) {
+            Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+            if (rb != null) {
+                rb.AddExplosionForce(explosionForce, position, explosionRadius);
+            }
+        }
     }
 
     override public void OnStartAuthority() {
